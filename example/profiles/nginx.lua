01fcbb4d55ea34d98b752353b5b40d4e9bfe725b
MrNavaStar <mr.navastar@gmail.com>
2026-08-22 21:42:38 -0600

    try new image

--- example/profiles/nginx.lua
+++ example/profiles/nginx.lua
@@ -1,4 +1,5 @@
 return {
-    image = "oci://docker.io/library/nginx:trixie-perl",
+    --image = "oci://docker.io/library/nginx:trixie-perl",
+    image = "oci://ghcr.io/freemkv/autorip:latest";
     kernel = "file:///vmlinuz"
 }
